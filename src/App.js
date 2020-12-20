ec80cd8971111d8871a38879feb8c7bf7be96389
Jordan Handy <handy.jordan@gmail.com>
2020-12-20 07:42:01 -0500

    remove dependency on robots.txt file.  Now using API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 // import deconstructed { Component } from 'react'
 import React, { Component } from "react";
 import CardList from "./CardList";
-import SearchBox from './SearchBox';
-import { robots } from "./robots";
-import './App.css'
+import SearchBox from "./SearchBox";
+import "./App.css";
 
 // Basic construct of State in react
 // Your App extends the Component class
@@ -17,13 +16,27 @@ import './App.css'
 class App extends Component {
   // call the base class
   // call super
-  constructor(){
-    super()
+  constructor() {
+    super();
     // define the 'state' object
     this.state = {
-      robots: robots,
-      searchfield: ''
-    }
+      robots: [],
+      searchfield: "",
+    };
+  }
+  // use React lifecycle method to call componentDidMount() and create a GET
+  // request to an API endpoint.  This endpoint holds the data we need, so the static
+  // robots.txt data file is not needed
+
+  // JSON-formatted response is returned, then this is set as the new state of the 'robots' prop
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
   }
 
   // the function action on state.
@@ -31,22 +44,24 @@ class App extends Component {
   // knows that you are referring to the parent, and not the event in question, sent as params
   onSearchChange = (event) => {
     // set the state of the searchfield to equal that of the value being typed into the searchfield
-    this.setState({searchfield: event.target.value})
-  }
+    this.setState({ searchfield: event.target.value });
+  };
 
   render() {
     // your constant then becomes a filtered list of robots, based on what is put into the searchfield
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    })
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
     return (
-    <div className="tc">
-    <h1 className="f1">RoboFriends</h1>
-    {/* the onSearchChange prop is sent to the searchBox */}
-    <SearchBox searchChange={this.onSearchChange} />
-    {/* instead of a constant list of 10 robots, you then set the value of robots to be the list of filteredRobots */}
-   <CardList robots={filteredRobots} />
-   </div>
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        {/* the onSearchChange prop is sent to the searchBox */}
+        <SearchBox searchChange={this.onSearchChange} />
+        {/* instead of a constant list of 10 robots, you then set the value of robots to be the list of filteredRobots */}
+        <CardList robots={filteredRobots} />
+      </div>
     );
   }
 }
