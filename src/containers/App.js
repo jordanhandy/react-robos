ccf8e3b61c92e4f409d08a8965586017805c178c
Jordan Handy <handy.jordan@gmail.com>
2020-12-20 11:07:09 -0500

    implemented hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,11 @@
 // import deconstructed { Component } from 'react'
-import React, { Component } from "react";
+// Import hooks useState, and useEffect
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 // Error Boundary introduced in React 16
-import ErrorBoundry from "../components/ErrorBoundary";
+import ErrorBoundry from "./ErrorBoundary";
 import "./App.css";
 
 // Basic construct of State in react
@@ -16,64 +17,50 @@ import "./App.css";
 
 // In this case, it is the robots data file, and the
 // search field acting on that data file
-class App extends Component {
-  // call the base class
-  // call super
-  constructor() {
-    super();
-    // define the 'state' object
-    this.state = {
-      robots: [],
-      searchfield: "",
-    };
-  }
-  // use React lifecycle method to call componentDidMount() and create a GET
-  // request to an API endpoint.  This endpoint holds the data we need, so the static
-  // robots.txt data file is not needed
+function App() {
+  const [robots, setRobots] = useState([])
+  const [searchfield, setSearchField] = useState("");
+  // use useEffect to run when the app loads, to grab robot data
 
   // JSON-formatted response is returned, then this is set as the new state of the 'robots' prop
-  componentDidMount() {
+
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => {
-        return response.json();
-      })
-      .then((users) => {
-        this.setState({ robots: users });
+      .then(response => response.json())
+      .then(users => {
+        setRobots(users)
       });
-  }
+  }, [])
+  // run initially only when the component mounts
 
   // the function action on state.
   // You need to use the functionName = (params) => { syntax so that when you call 'this', the app
   // knows that you are referring to the parent, and not the event in question, sent as params
-  onSearchChange = (event) => {
+  const onSearchChange = (event) => {
     // set the state of the searchfield to equal that of the value being typed into the searchfield
-    this.setState({ searchfield: event.target.value });
+    setSearchField(event.target.value);
   };
-
-  render() {
-    const { robots, searchfield } = this.state;
-    // your constant then becomes a filtered list of robots, based on what is put into the searchfield
-    const filteredRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    });
-    // ternary operator.  If robots.length is equal to 0, display "Loading..."
-    // if not, display the robots
-    return !robots.length ? (
-      <h1>Loading...</h1>
-    ) : (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        {/* the onSearchChange prop is sent to the searchBox */}
-        <SearchBox searchChange={this.onSearchChange} />
-        {/* instead of a constant list of 10 robots, you then set the value of robots to be the list of filteredRobots */}
-        {/* Wrap our card list in the Error Boundary to catch if anything went wrong */}
-        <ErrorBoundry>
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </ErrorBoundry>
-      </div>
-    );
-  }
+  // your constant then becomes a filtered list of robots, based on what is put into the searchfield
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  })
+  // ternary operator.  If robots.length is equal to 0, display "Loading..."
+  // if not, display the robots
+  return !robots.length ? (
+    <h1>Loading...</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">RoboFriends</h1>
+      {/* the onSearchChange prop is sent to the searchBox */}
+      <SearchBox searchChange={onSearchChange} />
+      {/* instead of a constant list of 10 robots, you then set the value of robots to be the list of filteredRobots */}
+      {/* Wrap our card list in the Error Boundary to catch if anything went wrong */}
+      <ErrorBoundry>
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </ErrorBoundry>
+    </div>
+  );
 }
 export default App;
